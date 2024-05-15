684ab75d537d7542f52a0ae840052731e61eb274
vladIsTrue <vladislavurcev@gmail.com>
2024-05-15 15:28:19 +0300

    output for user

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 int main(int argc, char* argv[])
 {
+    // мб докрутить мануал сюда
+    // сначал передаем, что ищем, потом то, где ищем, вот и все
+    // нужно смотреть, чтобы коэффициент располагался в интервале от 1 до 2, чем ближе к двум, тем лучше
+    // , он может немного вылазить, но это нужно обработать
+
     if (argc < 3)
     {
         std::cout << "Enter the names of the two files\n";
@@ -11,12 +16,22 @@ int main(int argc, char* argv[])
     auto [vector, min] = test::mid_to_vector(argv[1]);
     auto [vector1, min1] = test::mid_to_vector(argv[2], min);
 
-    std::cout << "\n\n\n";
+    //std::cout << algo::lcs(vector.begin(), vector.end(), vector1.begin(), vector1.end()) << "\n";
+    double lcs_mod = algo::lcs_mod(vector.begin(), vector.end(), vector1.begin(), vector1.end());
 
-    std::cout << algo::lcs(vector.begin(), vector.end(), vector1.begin(), vector1.end()) << "\n";
+    double coeff = lcs_mod / vector.size();
 
+    std::cout << lcs_mod << "\n";
     std::cout << '\n' << vector.size() << '\n';
-    std::cout << vector1.size() << '\n';
+    std::cout << coeff << "\n";
+
+    double error = 0.05;
+    const double left = 1, right = 2;
+
+    if (coeff >= left + error && coeff <= right + error)
+        std::cout << "Match found!!!\n";
+    else
+        std::cout << "No match found\n";
 
     return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -46,6 +46,43 @@ std::tuple<std::vector<int>, double> mid_to_vector(const std::string& filename, 
     return {symbol_str, min};
 }
 
+std::tuple<std::vector<int>, double> mid_to_vector1(const std::string& filename, double min_duration = INTMAX_MAX)
+{
+    using namespace smf;
+
+    MidiFile midifile;
+
+    midifile.readSmf(filename);
+
+    midifile.doTimeAnalysis();
+    midifile.linkNotePairs();
+
+    int track = 1;
+    double min = min_duration;  // потом можно "усложнить"
+    for (int event = 0; event < midifile[track].size(); ++event) {
+        if (midifile[track][event].isNoteOn()) {
+
+            if (midifile[track][event].getDurationInSeconds() < min)
+                min = midifile[track][event].getDurationInSeconds();
+        }
+    }
+
+    std::vector<int> symbol_str;
+
+    for (int event = 0; event < midifile[track].size(); ++event) {
+        if (midifile[track][event].isNoteOn()) {
+
+            int number = midifile[track][event].getDurationInSeconds() / min;
+
+            for (int i = 0; i < number; ++i) {
+                symbol_str.push_back((int)midifile[track][event][1]);
+            }
+        }
+    }
+
+    return {symbol_str, min};
+}
+
 }
 
 namespace algo
@@ -53,6 +90,37 @@ namespace algo
 
 template <class It>
 int lcs(It x_begin, It x_end, It y_begin, It y_end)
+{
+    auto m = std::distance(x_begin, x_end);
+    auto n = std::distance(y_begin, y_end);
+
+    std::vector<std::vector<int>> L(m + 1, std::vector<int>(n + 1));
+
+    for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+
+            else
+            {
+                auto[x, y] = std::make_tuple(x_begin, y_begin);
+                std::advance(x, i-1); std::advance(y, j-1);
+
+                if ( *x == *y )
+                    L[i][j] = L[i-1][j-1] + 1;
+                else
+                    L[i][j] = std::max( L[i-1][j], L[i][j-1] );
+            }
+
+        }
+    }
+
+    return L[m][n];
+}
+
+template <class It>
+int lcs_mod(It x_begin, It x_end, It y_begin, It y_end)
 {
     auto m = std::distance(x_begin, x_end);
     auto n = std::distance(y_begin, y_end);
