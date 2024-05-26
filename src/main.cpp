efd1f4a04b34bd6f5aed7e86b6af3ae9d6069e44
vladIsTrue <vladislavurcev@gmail.com>
2024-05-26 14:36:07 +0300

    remove comm

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,6 @@
 
 int main(int argc, char* argv[])
 {
-    // мб докрутить мануал сюда
-    // сначал передаем, что ищем, потом то, где ищем, вот и все
-    // нужно смотреть, чтобы коэффициент располагался в интервале от 1 до 2, чем ближе к двум, тем лучше
-    // , он может немного вылазить, но это нужно обработать
-
     if (argc < 3)
     {
         std::cout << "Enter the names of the two files\n";
@@ -16,14 +11,10 @@ int main(int argc, char* argv[])
     auto [vector, min] = test::mid_to_vector(argv[1]);
     auto [vector1, min1] = test::mid_to_vector(argv[2], min);
 
-    //std::cout << algo::lcs(vector.begin(), vector.end(), vector1.begin(), vector1.end()) << "\n";
+    //double lcs = algo::lcs(vector.begin(), vector.end(), vector1.begin(), vector1.end());
     double lcs_mod = algo::lcs_mod(vector.begin(), vector.end(), vector1.begin(), vector1.end());
 
     double coeff = lcs_mod / vector.size();
-
-    std::cout << lcs_mod << "\n";
-    std::cout << '\n' << vector.size() << '\n';
-    std::cout << coeff << "\n";
 
     double error = 0.05;
     const double left = 1, right = 2;
